67754660c1d0715bfbbf8a085620cfad153c852c
Erik Dittrich <erik.dittrich@gmail.com>
2025-11-09 20:09:46 +0100

    Initial

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
 
 load_dotenv()
 
@@ -29,6 +30,7 @@ def main():
     )
 
     llm = ChatOpenAI(temperature=0, model="gpt-5")
+    llm = ChatOllama(temperature=0, model="gpt-oss:20b")
     chain = summary_prompt_template | llm
 
     response = chain.invoke(input={"information": information})
