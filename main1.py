4f7d4c91a5b36f17015e53f7f3f27126db392d0f
Erik Dittrich <erik.dittrich@gmail.com>
2025-11-15 18:59:13 +0100

    Initial

--- main1.py
+++ main1.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
-from langchain_openai import ChatOpenAI
 from langchain_ollama import ChatOllama
+from langchain_openai import ChatOpenAI
 
 load_dotenv()
 
